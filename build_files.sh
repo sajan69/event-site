09052723529c34d955496e55757e04436901103e
Sajan Adhikari <120996556+sajan69@users.noreply.github.com>
2024-11-12 20:02:01 +0545

    feat:initializing the event-Site for vercel deploymenet test

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,13 @@
 echo " BUILD START"
 
-python3.10 --version
-python3.10 -m ensurepip # não aparece nos tutoriais internacionais
-python3.10 -m pip install -r requirements.txt
+python3.9 --version
+python3.9 -m ensurepip # não aparece nos tutoriais internacionais
+python3.9 -m pip install -r requirements.txt
 
 echo " MAKE MIGRATIONS..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 
-python3.10 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
